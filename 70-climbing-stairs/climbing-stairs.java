439090514717c660b0f45a3e51616899997e58c7
Dharani R <144825716+rolladharani@users.noreply.github.com>
2025-09-11 20:28:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (51.18%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=1)
+        return n;
+        int a=0,b=1,c;
+        for(int i=1;i<=n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+
+        }
+        return b;
+    }
+}
